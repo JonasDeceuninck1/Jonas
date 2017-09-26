a8964447bad01c44d655425618c704b9a703a548
JonasDeceuninck1 <jonas.deceuninck@student.howest.be>
2017-09-26 09:43:16 +0200

    dit is een eerste verandering

--- Oef2.py
+++ Oef2.py
@@ -4,3 +4,4 @@ hoogte = float(input("wat is the hoogte?"))
 oppervlakte = (basis * hoogte) /2
 
 print("de oppervlakte van u driehoek is {0: 0.2f}".format(oppervlakte))
+print("de oppervlakte van jouw driehoek is {0: 0.2f}".format(oppervlakte))
